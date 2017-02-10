8c61cbc34b1f037303be7a99d0bc084cb6e6c749
ezra <equilibriumarts@gmail.com>
2017-02-10 09:06:19 -0800

    pass rspec for creating new product

---
+++ spec/features/add_product_pages_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "the add a product process" do
+  it "adds a new product" do
+    visit products_path
+    click_link 'New Product'
+    fill_in 'Name', :with => 'Red Gummy'
+    fill_in 'Cost', :with => '3.33'
+    fill_in 'Country', :with => 'Taiwan'
+    fill_in 'Image', :with => 'http://www.heyheycandy.co.nz/616-1310-thickbox/red-gummy-bears.jpg'
+    click_on 'Create Product'
+    expect(page).to have_content 'Products'
+  end
+
+  it "gives error when no name is entered" do
+    visit new_product_path
+    click_on 'Create Product'
+    expect(page).to have_content 'errors'
+  end
+end
