1ba49b20e03afbac60fcc1b114300fccdf21136a
ezra <equilibriumarts@gmail.com>
2017-02-10 11:56:12 -0800

    adjust css styling

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "the add a product process" do
   it "adds a new product" do
     visit products_path
-    click_link 'Create New Product'
+    click_link 'create a new product'
     fill_in 'Name', :with => 'Red Gummy'
     fill_in 'Cost', :with => 3
     fill_in 'Country', :with => 'Taiwan'
